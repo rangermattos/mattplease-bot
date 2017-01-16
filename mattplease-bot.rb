3f59d3efda173900a40b2dcd2b2e42a987d2d319
rangermattos <rangermattos@protonmail.com>
2017-01-16 15:31:28 -0600

    Made only me able to use

--- mattplease-bot.rb
+++ mattplease-bot.rb
@@ -26,18 +26,22 @@ bot = Discordrb::Commands::CommandBot.new token: settings['token'], client_id: s
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
-bot.command :admin do |event|
-	break unless event.user.id == settings['owner']
-	bot.send_message(event.channel.id, 'https://discordapp.com/oauth2/authorize?client_id=240120547399303168&scope=bot&permissions=251132938')
-end
-
-bot.command :test do |event|
-	bot.send_message(event.channel.id, 'test')
-end
+#~bot.command :admin do |event|
+	#~break unless event.user.id == settings['owner']
+	#~bot.send_message(event.channel.id, 'https://discordapp.com/oauth2/authorize?client_id=240120547399303168&scope=bot&permissions=251132938')
+#~end
+#~
+#~bot.command :test do |event|
+	#~bot.send_message(event.channel.id, 'test')
+#~end
 
 bot.command :mattplease do |event|
+	break unless event.user.id == settings['owner'] # only i can use
 	channel = event.user.voice_channel
-	next "You're not in a voice channel" unless channel
+	break unless channel # only in voice chat
+	#~channel.users.each do |x|
+		#~return if x.id == 104428065626533888 # not if ray's in voice
+	#~end
 	voice_bot = bot.voice_connect(channel)
 	puts "Connected to channel: #{channel.name}"
 	
@@ -47,39 +51,43 @@ bot.command :mattplease do |event|
 	nil
 end
 
-bot.command :ignore do |event, usr|
-	usr_to_ignore = bot.parse_mention(usr)
-	if bot.ignored?(usr_to_ignore)
-		bot.send_message(event.channel.id, "User " + usr + " already ignored")
-	else
-		bot.send_message(event.channel.id, "Ignoring " + usr)
-		bot.ignore_user(usr_to_ignore)
-	end
-	nil
-end
-
-# currently not working, TODO
-bot.command :ignoredusers do |event|
-	return
-	ignored_ids = bot.instance_variable_get(:@ignored_ids).to_a
-	puts ignored_ids.to_s
-	ignored_users = ignored_ids.map do |id| 
-		p id
-		Discordrb::API::User.resolve(settings['token'],id)
-	end
-	bot.send_message(event.channel.id, ignored_users.to_s)
-end
-
-bot.command :unignore do |event, usr|
-	usr_to_unignore = bot.parse_mention(usr)
-	if !bot.ignored?(usr_to_unignore)
-		bot.send_message(event.channel.id, "User " + usr + " already unignored")
-	else
-		bot.send_message(event.channel.id, "Unignoring " + usr)
-		bot.unignore_user(usr_to_unignore)
-	end
-	nil
-end
+#~bot.command :ignore do |event, usr|
+	#~is_admin = event.author.permission?(:administrator);
+	#~break unless is_admin
+	#~usr_to_ignore = bot.parse_mention(usr)
+	#~if bot.ignored?(usr_to_ignore)
+		#~bot.send_message(event.channel.id, "User " + usr + " already ignored")
+	#~else
+		#~bot.send_message(event.channel.id, "Ignoring " + usr)
+		#~bot.ignore_user(usr_to_ignore)
+	#~end
+	#~nil
+#~end
+#~
+#~# currently not working, TODO
+#~bot.command :ignoredusers do |event|
+	#~return
+	#~ignored_ids = bot.instance_variable_get(:@ignored_ids).to_a
+	#~puts ignored_ids.to_s
+	#~ignored_users = ignored_ids.map do |id| 
+		#~p id
+		#~Discordrb::API::User.resolve(settings['token'],id)
+	#~end
+	#~bot.send_message(event.channel.id, ignored_users.to_s)
+#~end
+#~
+#~bot.command :unignore do |event, usr|
+	#~is_admin = event.author.permission?(:administrator);
+	#~break unless is_admin
+	#~usr_to_unignore = bot.parse_mention(usr)
+	#~if !bot.ignored?(usr_to_unignore)
+		#~bot.send_message(event.channel.id, "User " + usr + " already unignored")
+	#~else
+		#~bot.send_message(event.channel.id, "Unignoring " + usr)
+		#~bot.unignore_user(usr_to_unignore)
+	#~end
+	#~nil
+#~end
 
 bot.command(:exit, help_available: false) do |event|
   # This is a check that only allows a user with a specific ID to execute this command. Otherwise, everyone would be
@@ -87,6 +95,7 @@ bot.command(:exit, help_available: false) do |event|
   break unless event.user.id == settings['owner']
 
   bot.send_message(event.channel.id, 'Bot is shutting down')
+  bot.stop
   exit
 end
 
